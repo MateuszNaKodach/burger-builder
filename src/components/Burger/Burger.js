80f98be0046e22cdec0e4a68f39614bb7f21e82b
mnowak <mateusz.nowak@intenso.pl>
2018-02-09 00:50:25 +0100

    Dynamic burger ingredients.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import styles from './Burger.css';
 import BurgerIngredient, { BREAD_TOP, BREAD_BOTTOM, CHEESE, MEAT } from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [... Array(props.ingredients[igKey])].map((_,i) => {
+                return <BurgerIngredient key ={igKey+i} type={igKey}/>
+            })
+        });
+        
     return (
         <div className={styles.Burger}>
             <BurgerIngredient type={BREAD_TOP}/>
-            <BurgerIngredient type={CHEESE}/>
-            <BurgerIngredient type={MEAT}/>
+            {transformedIngredients}
             <BurgerIngredient type={BREAD_BOTTOM}/>
         </div>
     );
