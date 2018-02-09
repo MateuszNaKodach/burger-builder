e3cef58da394c2dc7b825e92fe5523d538c4d824
mnowak <mateusz.nowak@intenso.pl>
2018-02-09 23:48:29 +0100

    Add Modal with backdrop.

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import { SALAD, BACON, CHEESE, MEAT } from '../BurgerIngredient/BurgerIngredient';
+import {SALAD, BACON, CHEESE, MEAT} from '../BurgerIngredient/BurgerIngredient';
 
 import BuildControl from './BuildControl/BuildControl';
 import styles from './BuildControls.css';
 
 const controls = [
-    { label: 'Salad', type: SALAD },
-    { label: 'Bacon', type: BACON },
-    { label: 'Cheese', type: CHEESE },
-    { label: 'Meat', type: MEAT }
+    {label: 'Salad', type: SALAD},
+    {label: 'Bacon', type: BACON},
+    {label: 'Cheese', type: CHEESE},
+    {label: 'Meat', type: MEAT}
 ]
 
 const buildControls = (props) => (
     <div className={styles.BuildControls}>
-    <p>Current Price: <strong>{props.currentPrice.toFixed(2)}</strong></p>
+        <p>Current Price: <strong>{props.currentPrice.toFixed(2)}</strong></p>
         {controls.map(ctrl => <BuildControl
             key={ctrl.label}
             {...ctrl}
-            added={() => props.ingredientAdded(ctrl.type)}ł
-            removed={()=>props.ingredientRemoved(ctrl.type)}
-            disabled={props.disabled[ctrl.type]} />)
+            added={() => props.ingredientAdded(ctrl.type)} ł
+            removed={() => props.ingredientRemoved(ctrl.type)}
+            disabled={props.disabled[ctrl.type]}/>)
         }
+        <button
+            className={styles.OrderButton}
+            disabled={!props.purchasable}
+            onClick={props.ordered}>ORDER NOW
+        </button>
     </div>
 )
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@
 import React, { Component, Fragment } from 'react';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 export const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -19,7 +21,16 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false,
+        purchasing: false
+    }
+
+    updatePurchasableState(ingredients) {
+        const sum = Object.keys(ingredients)
+            .map(igKey => ingredients[igKey])
+            .reduce((sum,el) => sum + el,0);
+        this.setState({purchasable: sum>0});
     }
 
     addIngredientHandler = (type) => {
@@ -35,6 +46,7 @@ class BurgerBuilder extends Component {
         this.setState({
             totalPrice: newPrice, ingredients: updatedInredients
         })
+        this.updatePurchasableState(updatedInredients);
     }
 
     removeIngredientHandler = (type) => {
@@ -53,7 +65,16 @@ class BurgerBuilder extends Component {
         this.setState({
             totalPrice: newPrice, ingredients: updatedInredients
         })
-    }
+        this.updatePurchasableState(updatedInredients);
+    };
+
+    purchaseHandler = () => {
+        this.setState({purchasing: true});
+    };
+
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false});
+    };
 
     render() {
         const disabledInfo = {
@@ -66,12 +87,17 @@ class BurgerBuilder extends Component {
 
         return (
             <Fragment>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                    <OrderSummary ingredients={this.state.ingredients}/>
+                </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                     currentPrice={this.state.totalPrice}
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
-                    disabled={disabledInfo} />
+                    disabled={disabledInfo}
+                    ordered={this.purchaseHandler}
+                    purchasable={this.state.purchasable}/>
             </Fragment>
         );
     }
